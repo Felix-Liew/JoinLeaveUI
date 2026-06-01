7ea4139fca7834ec11362c89c5017dd552b68355
Felix Liew <changqing.se@gmail.com>
2026-06-01 23:53:52 +0800

    Add files via upload

---
+++ JoinLeaveUI/JoinLeaveUIConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Rocket.API;
+
+namespace FelixLiew.JoinLeaveUI
+{
+    public class JoinLeaveUIConfiguration : IRocketPluginConfiguration
+    {
+        public void LoadDefaults()
+        {
+
+        }
+    }
+}
